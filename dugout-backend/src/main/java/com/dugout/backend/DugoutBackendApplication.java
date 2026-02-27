2ebd42b6225cbc486fb29fd821dcda899b741946
Srivathsa Mantrala <svmantra@ncsu.edu>
2026-02-27 16:24:17 -0500

    Configured backend module and setup Postgres connection

---
+++ dugout-backend/src/main/java/com/dugout/backend/DugoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dugout.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DugoutBackendApplication.class, args);
+	}
+
+}
---
+++ dugout-backend/src/test/java/com/dugout/backend/DugoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dugout.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugoutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
